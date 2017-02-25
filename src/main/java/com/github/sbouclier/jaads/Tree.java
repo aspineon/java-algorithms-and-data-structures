093cc3fa838e529982837fadb4c5868288d0d3aa
Stéphane Bouclier <sbouclier@users.noreply.github.com>
2017-02-25 15:35:33 +0100

    add Tree

---
+++ src/main/java/com/github/sbouclier/jaads/Tree.java
@@ -0,0 +1,61 @@
+package com.github.sbouclier.jaads;
+
+/**
+ * A tree is a hierarchical data without cycle which contains nodes. The first
+ * tree node (without parent) is called 'root node'.
+ * 
+ * @author Stéphane Bouclier
+ *
+ * @param <T>
+ *            type of tree node value
+ */
+public class Tree<T> {
+
+	protected Node<T> root;
+
+	public Tree(Node<T> root) {
+		this.root = root;
+	}
+
+	public void print() {
+		print(root, "", true);
+	}
+
+	/**
+	 * Recursive algorithm for printing nodes
+	 * 
+	 * @param node
+	 *            current node
+	 * @param prefix
+	 *            to print before node value
+	 * @param isLastNode
+	 *            last node of parent node
+	 */
+	private void print(Node<T> node, String prefix, boolean isLastNode) {
+		if (node != null) {
+			String nextPrefix = prefix + (isLastNode ? "    " : "│   ");
+
+			if (node == root) {
+				prefix = nextPrefix = "";
+				System.out.println(prefix + node.value);
+			} else {
+				System.out.println(prefix + (isLastNode ? "└── " : "├── ") + node.value);
+			}
+
+			// print all nodes except last
+			for (int i = 0; i < node.children.size() - 1; i++) {
+				print(node.children.get(i), nextPrefix, false);
+			}
+
+			// print last node
+			if (node.children.size() > 0) {
+				print(node.children.get(node.children.size() - 1), nextPrefix, true);
+			}
+		}
+	}
+
+	@Override
+	public String toString() {
+		return "Tree [root=" + root + "]";
+	}
+}
---
+++ src/test/java/com/github/sbouclier/jaads/TreeTest.java
@@ -0,0 +1,84 @@
+package com.github.sbouclier.jaads;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Tree test
+ * 
+ * @author Stéphane Bouclier
+ *
+ */
+public class TreeTest {
+
+	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+	@Before
+	public void setUpStreams() {
+		System.setOut(new PrintStream(outContent));
+	}
+
+	@After
+	public void cleanUpStreams() {
+		System.setOut(null);
+	}
+
+	@Test
+	public void should_well_print_tree() {
+		// level 1
+		Node<String> root = new Node<>("1");
+
+		// level 2
+		Node<String> node11 = new Node<>("1.1");
+		Node<String> node12 = new Node<>("1.2");
+		root.addChild(node11);
+		root.addChild(node12);
+
+		// level 3
+		Node<String> node121 = new Node<>("1.2.1");
+		Node<String> node122 = new Node<>("1.2.2");
+		Node<String> node123 = new Node<>("1.2.3");
+		node12.addChild(node121);
+		node12.addChild(node122);
+		node12.addChild(node123);
+
+		// level 4
+		Node<String> node1221 = new Node<>("1.2.2.1");
+		node122.addChild(node1221);
+
+		Tree<String> tree = new Tree<>(root);
+		tree.print();
+
+		StringBuilder expectedOut = new StringBuilder("1").append("\n");
+		expectedOut.append("├── 1.1").append("\n");
+		expectedOut.append("└── 1.2").append("\n");
+		expectedOut.append("    ├── 1.2.1").append("\n");
+		expectedOut.append("    ├── 1.2.2").append("\n");
+		expectedOut.append("    │   └── 1.2.2.1").append("\n");
+		expectedOut.append("    └── 1.2.3").append("\n");
+
+		assertEquals(expectedOut.toString(), outContent.toString());
+	}
+
+	@Test
+	public void should_print_nothing_without_node() {
+		Tree<String> tree = new Tree<>(null);
+		tree.print();
+
+		assertEquals("", outContent.toString());
+	}
+
+	@Test
+	public void should_get_to_string() {
+		Node<Integer> root = new Node<>(1);
+		Tree<Integer> tree = new Tree<>(root);
+
+		assertEquals("Tree [root=Node [maxDegree=32767, value=1, children.size=0]]", tree.toString());
+	}
+}
